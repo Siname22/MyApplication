da99da96eef443f0257d495bdb64be8eddcb6618
HP196\CES <informatica@escuelaces.com>
2022-02-01 14:09:37 +0100

    Fragments, quintuple intento

--- app/src/main/java/com/example/myapplication/pokedex/PokemonInfo.java
+++ app/src/main/java/com/example/myapplication/pokedex/PokemonInfo.java
@@ -1,6 +1,8 @@
 package com.example.myapplication.pokedex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,48 +11,115 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.myapplication.R;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 import com.squareup.picasso.Picasso;
 
-public class PokemonInfo extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class PokemonInfo extends AppCompatActivity
+{
 
     TextView nombrePokemon;
     TextView codePokemon;
     ImageView imagenPokemon;
-
-
+    TabItem statsTab;
+    TabLayout tabLayout;
+    FragmentManager manager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon_info);
-        nombrePokemon = findViewById(R.id.namePokemonInfo);
-        codePokemon = findViewById(R.id.codePokemon);
-        imagenPokemon = findViewById(R.id.imagePokemonInfo);
-
-
-        Intent i = getIntent();
-        String  datos = i.getStringExtra("datos");
-        Log.i("datos",datos );
-
-        JsonObject infoPokemon = JsonParser.parseString(datos).getAsJsonObject();
-        infoPokemon.get("sprites").getAsJsonObject().get("other").getAsJsonObject().
-                get("official-artwork").getAsJsonObject().get("front_default").getAsString();
-
-        Log.i("imagen", infoPokemon.get("sprites").getAsJsonObject().get("other").
-                getAsJsonObject().get("official-artwork").getAsJsonObject().get("front_default").
-                getAsString());
-
-        Picasso.get().load(infoPokemon.get("sprites").getAsJsonObject().get("other").
-                getAsJsonObject().get("official-artwork").getAsJsonObject().get("front_default").
-                getAsString()).into(imagenPokemon);
-        nombrePokemon.setText(infoPokemon.get("name").getAsString());
-        Log.i("nombre", infoPokemon.get("name").getAsString());
-
-        codePokemon.setText(infoPokemon.get("id").getAsString());
-        Log.i("codigo", infoPokemon.get("id").getAsString());
-
+        try {
+            consumirAPI();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
+
+        void consumirAPI() throws JSONException {
+            String pkmName;
+            int pesoPokemon = 0;
+            String hp;
+            nombrePokemon = findViewById(R.id.namePokemonInfo);
+            codePokemon = findViewById(R.id.codePokemon);
+            imagenPokemon = findViewById(R.id.imagePokemonInfo);
+            tabLayout = findViewById(R.id.tabLayout);
+            statsTab = tabLayout.findViewById(R.id.stats);
+            manager = getSupportFragmentManager();
+            Intent i = getIntent();
+            String  datos = i.getStringExtra("datos");
+            Log.i("datos",datos );
+
+            JSONObject infoPokemon = null;
+                infoPokemon = new JSONObject(datos);
+                pkmName = infoPokemon.getString("name");
+                pesoPokemon = infoPokemon.getInt("weight");
+                //hp = infoPokemon.getJSONObject()
+
+            //int statsHp = infoPokemon.get("stats").getAsJsonObject().get("base_stat").getAsInt();
+            JSONArray arrayStats = infoPokemon.getJSONArray("stats");
+            for (int j = 0; j < arrayStats.length(); j++) {
+                System.out.println(arrayStats.getJSONObject(j).getInt("base_stat"));
+                System.out.println(arrayStats.getJSONObject(j).getJSONObject("stat").getString("name"));
+            }
+
+            Log.i("peso", String.valueOf(pesoPokemon));
+            //Log.i("hp", hp);
+            //Log.i("stats", String.valueOf(statsHp));
+            Picasso.get().load(
+                    infoPokemon.getJSONObject("sprites").
+                            getJSONObject("other").
+                            getJSONObject("official-artwork").getString("front_default")
+            ).into(imagenPokemon);
+
+
+            nombrePokemon.setText(pkmName);
+
+            Log.i("nombre", pkmName);
+
+            codePokemon.setText(infoPokemon.getString("id"));
+            Log.i("codigo", infoPokemon.getString("id"));
+            tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    if(tab.getPosition() == 0)
+                    {
+                        System.out.println("HOLAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                        FragmentTransaction transaction = manager.beginTransaction();
+                        PokemonFragment fragment = PokemonFragment.newInstance(pkmName);
+                        transaction.replace(R.id.fragmentContainerView, fragment).commit();
+                    }
+                }
+
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
+                    if(tab.getPosition() == 1)
+                    {
+                        System.out.println("HOLAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                        FragmentTransaction transaction = manager.beginTransaction();
+                        PokemonStatsFragment fragment = new PokemonStatsFragment();
+                        transaction.replace(R.id.fragmentContainerView, fragment).commit();
+                    }
+                }
+
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
+                    if(tab.getPosition() == 2)
+                    {
+                        System.out.println("HOLAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                        FragmentTransaction transaction = manager.beginTransaction();
+                        //PokemonHabilidadesFragment fragment = new PokemonHabilidadesFragment();
+                        //transaction.replace(R.id.fragmentContainerView, fragment).commit();
+                    }
+                }
+            });
+        }
+
+
+
 }
--- app/src/main/java/com/example/myapplication/pokedex/PokemonStatsFragment.java
+++ app/src/main/java/com/example/myapplication/pokedex/PokemonStatsFragment.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.myapplication.pokedex;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.myapplication.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PokemonFragment#newInstance} factory method to
+ * Use the {@link PokemonStatsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PokemonFragment extends Fragment {
+public class PokemonStatsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class PokemonFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public PokemonFragment() {
+    public PokemonStatsFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class PokemonFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PokemonFragment.
+     * @return A new instance of fragment PokemonStatsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static PokemonFragment newInstance(String param1, String param2) {
-        PokemonFragment fragment = new PokemonFragment();
+    public static PokemonStatsFragment newInstance(String param1, String param2) {
+        PokemonStatsFragment fragment = new PokemonStatsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class PokemonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokemon, container, false);
+        return inflater.inflate(R.layout.fragment_pokemon_stats, container, false);
     }
 }
