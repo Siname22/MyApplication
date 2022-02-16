5cadd23686f1dcfdde7fd3a77aaa6cb82f1c4a2f
luchosaque <luchosaque@gmail.com>
2022-02-16 17:32:22 +0100

    Luis->haciendo el login

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,31 +6,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.example.myapplication.pokedex.Pokedex;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button info;
+    Button log;
+    Button register;
+    EditText name;
+    EditText email;
+    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            }
-        });
-        info = findViewById(R.id.info);
-        info.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                Intent b = new Intent(MainActivity.this,Pokedex.class);//cambiar a la activity de info
-                startActivity(b);
+        email=findViewById(R.id.EmailAddress);
+        name=findViewById(R.id.txtName);
+        password=findViewById(R.id.Password);
+
+        log=findViewById(R.id.login);
+        register=findViewById(R.id.register);
+
+
+
+    }
+    void activarButtons()
+    {
+        if (email.getText().equals("") | name.getText().equals("")|password.getText().equals(""))
+        {
+            log.setEnabled(true);
+            register.setEnabled(false);
+        }
     }
 
-    public void Advertencia(View view){
-        Intent ayuda = new Intent(this, Advertencia.class);
-        startActivity(ayuda);
-    }
+    public void Register(View view){
 
+        Intent registro = new Intent(this, Register.class);
+        startActivity(registro);
+    }
+    public void Login(View view){
+
+        Intent log = new Intent(this, Menu.class);
+        startActivity(log);
+    }
 }
---
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+
+    public void Login(View view){
+
+        Intent log = new Intent(this, Menu.class);
+        startActivity(log);
+    }
+}
